c838d184113ad777c8ef1c3981b0ffb7e124d296
Romedaa <gaioromeda@gmail.com>
2026-09-09 09:49:20 +0200

    bout it

--- Assets/Gort Folder/GortScripts/ShopScript.cs
+++ Assets/Gort Folder/GortScripts/ShopScript.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
 using NUnit.Framework;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class ShopScript : MonoBehaviour
 {
+    public List<GameObject> Items = new List<GameObject>();
     public List<Transform> ItemSlots = new List<Transform>();
     public List<Vector2Int> SlotCoordinates = new List<Vector2Int>();
 
@@ -15,53 +18,87 @@ public class ShopScript : MonoBehaviour
     public GameObject Book;
 
     public GameObject Indicator;
+    public int Columns = 5;
+    private int CurrentSlot = 0;
 
-    public int columns = 5;
-
-    private int currentSlot = 0;
+    public int Currency;
+    public Text CurrencyText;
     void Start()
     {
+        Currency = 100;
         RefreshShop();
         UpdateIndicator();
+        UpdateCostTexts();
     }
     void RefreshShop()
     {
         for (int i = 0; i < ItemSlots.Count; i++)
         {
+            GameObject newItem = new GameObject();
             int decider = Random.Range(1, 6);
             
             if (decider == 1)
             {
-                Instantiate(Potion, ItemSlots[i].position, ItemSlots[i].rotation);
+                newItem = Instantiate(Potion, ItemSlots[i].position, ItemSlots[i].rotation);
             }
             else if (decider == 2)
             {
-                Instantiate(Potion2, ItemSlots[i].position, ItemSlots[i].rotation);
+                newItem = Instantiate(Potion2, ItemSlots[i].position, ItemSlots[i].rotation);
             }
             else if (decider == 3)
             {
-                Instantiate(Helmet, ItemSlots[i].position, ItemSlots[i].rotation);
+                newItem = Instantiate(Helmet, ItemSlots[i].position, ItemSlots[i].rotation);
             }
             else if (decider == 4)
             {
-                Instantiate(Book, ItemSlots[i].position, ItemSlots[i].rotation);
+                newItem = Instantiate(Book, ItemSlots[i].position, ItemSlots[i].rotation);
             }
             else if (decider == 5)
             {
-                Instantiate(Sword, ItemSlots[i].position, ItemSlots[i].rotation);
+                newItem = Instantiate(Sword, ItemSlots[i].position, ItemSlots[i].rotation);
             }
+
+            Items.Add(newItem);
+        }
+        CurrencyText.text = "Currency: " + Currency;
+    }
+    void UpdateCostTexts()
+    {
+        for (int i = 0; i < ItemSlots.Count; i++)
+        {
+            if (i == CurrentSlot)
+            {
+                ItemSlots[i].GetChild(0).GetChild(0).GetComponent<Text>().text = "Cost: " + Items[CurrentSlot].GetComponent<ItemStatsScript>().Price;
+            }
+            else
+            {
+                ItemSlots[i].GetChild(0).GetChild(0).GetComponent<Text>().text = "";
+            }
+        }
+    }
+    void PlayerInput()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && Items[CurrentSlot] != null)
+        {
+            int cost = Items[CurrentSlot].GetComponent<ItemStatsScript>().Price;
+            if (Currency - cost > 0 )
+            {
+                Currency -= cost;
+                CurrencyText.text = "Currency: " + Currency;
+                Destroy(Items[CurrentSlot]);
+            }   
         }
     }
     void Move(Vector2Int direction)
     {
-        Vector2Int currentCoordinate = SlotCoordinates[currentSlot];
+        Vector2Int currentCoordinate = SlotCoordinates[CurrentSlot];
         Vector2Int targetCoordinate = currentCoordinate + direction;
 
         for (int i = 0; i < SlotCoordinates.Count; i++)
         {
             if (SlotCoordinates[i] == targetCoordinate)
             {
-                currentSlot = i;
+                CurrentSlot = i;
                 return;
             }
         }
@@ -88,10 +125,12 @@ public class ShopScript : MonoBehaviour
             Move(Vector2Int.down);
         }
 
-        Indicator.transform.position = ItemSlots[currentSlot].position;
+        Indicator.transform.position = ItemSlots[CurrentSlot].position;
     }
     void Update()
     {
+        PlayerInput();
         UpdateIndicator();
+        UpdateCostTexts();
     }
 }
