1bde952950a5da2ea67646bbba2fbc3b051bb6a7
bamboombox <olafslazynski@gmail.com>
2026-09-09 12:34:36 +0200

    moving player

--- Assets/Olf_Folder/Scripts/HolyGame_PlayerMovement.cs
+++ Assets/Olf_Folder/Scripts/HolyGame_PlayerMovement.cs
@@ -9,7 +9,7 @@ public class HolyGame_PlayerMovement : MonoBehaviour
     Rigidbody2D GodRB;
     public InputActionAsset InputActions;
     SpriteRenderer God_Sprite;
-
+    public int FacingDirection = 1;
 
     private InputAction G_MoveAction;
     private InputAction G_JumpAction;
@@ -21,6 +21,12 @@ public class HolyGame_PlayerMovement : MonoBehaviour
     public float G_MovementSpeed;
     public float G_JumpSpeed;
     public float G_DashSpeed;
+
+    private bool IsFacingRight;
+    private bool GroundCheckFrame;
+    public Transform GroundCheck;
+    public Vector2 GroundCheckBox = new Vector2(0.4f, 0.1f);
+    public LayerMask Groundmask;
 
     private void OnEnable()
     {
@@ -44,7 +50,57 @@ public class HolyGame_PlayerMovement : MonoBehaviour
     private void Update()
     {
         G_MoveAmt = G_MoveAction.ReadValue<Vector2>();
+        GroundCheckFrame = IsGrounded(Groundmask);
+
+        if (G_JumpAction.WasPressedThisFrame() && GroundCheckFrame)
+        {
+            Jump();
+        }
+
+        if (G_MoveAmt.x < 0f && !IsFacingRight)
+        {
+            Flip();
+        }
+        if (G_MoveAmt.x > 0f && IsFacingRight)
+        {
+            Flip();
+        }
     }
+
+    private bool IsGrounded(LayerMask groundLayer)
+    {
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(GroundCheck.position, GroundCheckBox, 0f, groundLayer);
+
+        foreach (var collider in colliders)
+        {
+            if (collider.gameObject == gameObject)
+                continue;
+            return true; 
+        }
+        return false;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        if (GroundCheck != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(GroundCheck.position, GroundCheckBox);
+        }
+    }
+
+    private void Flip()
+    {
+        IsFacingRight = !IsFacingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1f;
+        transform.localScale = scale;
+    }
+    private void Jump()
+    {
+        GodRB.linearVelocity = new Vector2(GodRB.linearVelocity.x, 0);
+        GodRB.AddForce(Vector2.up * G_JumpSpeed, ForceMode2D.Impulse);
+    }
+
     private void FixedUpdate()
     {
         float moveVelocityX = (G_MoveAmt.x * G_MovementSpeed);
